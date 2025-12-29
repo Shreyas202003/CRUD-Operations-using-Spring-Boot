a3a18557ab4e8ea38a2a63dbf6e092eb41c789fa
Shreyas202003 <shreyassalunkhe2003@gmail.com>
2025-12-29 08:50:20 +0530

    Changed the upload file path from static to dynamic using ClassPathResourse.

--- CURD_ON_Book/src/main/java/com/book/controller/FileIUploading.java
+++ CURD_ON_Book/src/main/java/com/book/controller/FileIUploading.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.book.helper.FileUploadHelper;
 
@@ -33,7 +34,10 @@ public class FileIUploading {
 		
 		boolean bool = fileHelper.fileUpload(file);
 		if(bool) {
-			return ResponseEntity.ok().body("File uploaded successfully");
+//			return ResponseEntity.ok().body("File uploaded successfully");
+			
+			//returning in the form of uri
+			return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(file.getOriginalFilename()).toUriString());
 		}
 		
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload the file");
--- CURD_ON_Book/src/main/java/com/book/helper/FileUploadHelper.java
+++ CURD_ON_Book/src/main/java/com/book/helper/FileUploadHelper.java
@@ -1,40 +1,59 @@
 package com.book.helper;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-	public static String UPLOAD_DIR = "D:\\STS workSpace (D)\\Git\\repository\\CURD_ON_Book\\src\\main\\resources\\static\\Image";
+	// Static file path..
+//	public static String UPLOAD_DIR = "D:\\STS workSpace (D)\\Git\\repository\\CURD_ON_Book\\src\\main\\resources\\static\\Image";
+
+	// Dynamic file path
+	public final String UPLOAD_DIR;
+
+    public FileUploadHelper() throws Exception {
+        File dir = new ClassPathResource("static/image/").getFile();
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        this.UPLOAD_DIR = dir.getAbsolutePath();
+    }
 	
+
+
 	public boolean fileUpload(MultipartFile file) {
-		boolean b=false;
+		boolean b = false;
 		try {
-			
-			//Old stream API method
-			//Read
-			InputStream is = file.getInputStream();
-			byte[] data = new byte[is.available()];
-			is.read(data);
-			//write
-			FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+file.getOriginalFilename());
-			fos.write(data);
-			
-			fos.flush();
-			fos.close();
-			b=true;
-			
-			
-		}
-		catch(Exception e) {
+
+			// Old stream API method
+			// Read
+//			InputStream is = file.getInputStream();
+//			byte[] data = new byte[is.available()];
+//			is.read(data);
+//			//write
+//			FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+file.getOriginalFilename());
+//			fos.write(data);
+//			
+//			fos.flush();
+//			fos.close();
+
+			// Using Stream API (NIO Package)
+			Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR + File.separator + file.getOriginalFilename()),
+					StandardCopyOption.REPLACE_EXISTING);
+
+			b = true;
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return b;
 	}
-	
+
 }
